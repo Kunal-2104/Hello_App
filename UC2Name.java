f1a1c89d5e65cba339c614bf1088413f8c7d6554
Kunal-2104 <kt8774@srmist.edu.in>
2026-03-25 13:13:03 +0530

    UC2: Confirmed implementation for Command Line Arguments

--- UC2Name.java
+++ UC2Name.java
@@ -1,12 +1,9 @@
 public class UC2Name {
     public static void main(String[] args) {
-        // Check if a command-line argument was provided
+        // UC2: Take name from command-line argument
         if (args.length > 0) {
-            // Join all arguments in case of a full name (e.g., "Kunal Thakur")
-            String name = String.join(" ", args);
-            System.out.println("Hello, " + name + "!");
+            System.out.println("Hello, " + args[0] + "!");
         } else {
-            // Fallback if no name is provided
             System.out.println("Hello World!");
         }
     }
